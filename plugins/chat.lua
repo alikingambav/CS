4ed91bc8e80c6ea3aadd02aec0adf2848921b93a
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-02-04 15:03:07 +0330

    Update chat.lua

--- plugins/chat.lua
+++ plugins/chat.lua
@@ -1,7 +1,6 @@
 local function run(msg)
 if msg.text == "ARJ" then
 	return "با باباییم چکار داری ؟ الان میارمش"
-        chat_add_user('member#45446970', ok_cb, false)
 end
 if msg.text == "A.R.J" then
 	return "با باباییم چکار داری ؟ الان میارمش"
