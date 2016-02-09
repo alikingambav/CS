d6057d9f699d6f3086fbe973d1d656a591627a65
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-02-09 22:02:44 +0330

    Update nas.lua

--- plugins/nas.lua
+++ plugins/nas.lua
@@ -1,10 +1,9 @@
 local function run(msg, matches)
-local c = 1
  if not is_momod(msg) then 
   return "For moderators only !"
  end
  if matches[1] then
-    for c = 1, 10 do
+    for c = 1,10 do
      print(c)
     end
  end
