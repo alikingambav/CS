490d7ad5d44367b4c9b8567242fbae7112d41368
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-02-18 17:31:00 +0330

    Update xy.lua

--- plugins/xy.lua
+++ plugins/xy.lua
@@ -2,14 +2,14 @@ do
 local function run(msg, matches)
 local x = 125775990
  if msg.text == "xy on" or msg.text == "Xy on" then
-    local xy = on
+    local xy = "on"
     return "XY ✅"
  elseif msg.test == "xy off" or msg.text "Xy off" then
-    local xy = off
+    local xy = "off"
     return "XY ❌"
  end
     if msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(x) then
-       if xy = on then
+       if xy = "on" then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..x, ok_cb, false)
        else
        return
