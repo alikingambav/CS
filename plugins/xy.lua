404c13df98729fc809d2415948239993172f54ba
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-02-18 16:11:28 +0330

    Update xy.lua

--- plugins/xy.lua
+++ plugins/xy.lua
@@ -1,14 +1,8 @@
 do
 local function run(msg, matches)
-local x = 45446970
-local y = 125775990
-local z = 136378403
+local x = 125775990
     if msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(x) then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..x, ok_cb, false)
-  elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(z) then
-       chat_add_user("chat#id"..msg.to.id, 'user#id'..z, ok_cb, false)
-  elseif msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(y) then
-       chat_add_user("chat#id"..msg.to.id, 'user#id'..y, ok_cb, false)
     end
 end
  
