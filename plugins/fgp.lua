6ca0739c54ab98f42ca668176d7741d55277008d
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-02-18 16:39:31 +0330

    Update fgp.lua

--- plugins/fgp.lua
+++ plugins/fgp.lua
@@ -1,1 +1,14 @@
+local function run(msg)
 
+end
+
+return {
+        description = "Fuck groups",
+        usage = "fuck groups",
+        patterns = {
+             "^[Ff]gp$",
+             "^[Ff]uckgp$"
+             },
+        run = run,
+        pre_process = pre_process
+        }
