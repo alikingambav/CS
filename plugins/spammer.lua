5c4ce2d17117bc5c0215173fb9afbdd63ea5fd86
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-02-18 22:59:53 +0330

    Update spammer.lua

--- plugins/spammer.lua
+++ plugins/spammer.lua
@@ -3,16 +3,18 @@ do
 function run(msg, matches)
        if not is_sudo(msg) then
               return "Only Sudoers Can Use it"       end
-  local num = matches[1]
-  local txt = matches[2]
+  local tex = matches[1]
+  local sps = matches[2]
 
-for i=1, num, 1 do
-
-s = '\n'..txt..'\n\n\n\n\n\n\n\n\n\n\n\n\n\n'..s
+for i=1, tex, 1 do
+return i
+sp = '\n'..sps..'\n'
 i = i + 1
 
 end
-return s
+
+return sp
+
 end
 
 return {
