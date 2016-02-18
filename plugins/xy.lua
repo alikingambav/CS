4b0352bf93a2afae50b60a3877a5bc150b2a13a7
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-02-18 17:53:08 +0330

    Update xy.lua

--- plugins/xy.lua
+++ plugins/xy.lua
@@ -10,10 +10,9 @@ local x = 125775990
     return "XY ❌"
  end
     if msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(x) then
-       if xy == on then
+       if xy == "on" then
        chat_add_user("chat#id"..msg.to.id, 'user#id'..x, ok_cb, false)
        else
-       return
        end
     end
 end
