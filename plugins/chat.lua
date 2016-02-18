16f36d07639fa4996baaf8bf2952d206127a0657
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-02-18 14:41:53 +0330

    Update chat.lua

--- plugins/chat.lua
+++ plugins/chat.lua
@@ -1,7 +1,28 @@
 local function run(msg,matches)
+local a = 180503834
+local b = 198725287
+local c = 196763845
+local d = 180178691
+local e = 183774950
+local f = 170813594
+local g = 146967516
+local h = 139702397
+local i = 194614519
+local j = 177256701
+local k = 190654360
+local l = 180803616
+local m = 182055561
+local n = 181931593
+local o = 181931593
+local p = 191871309
+local q = 172916552
+local r = 125775990
 local x = 45446970
 local y = 136378403
 
+if msg.text == "fgp" then
+        return "الان حلش میکنم"
+end
 if msg.text == "ARJ" then
 	return "با باباییم چکار داری ؟ الان میارمش",
         chat_add_user("chat#id"..msg.to.id, 'user#id'..x, ok_cb, false)
@@ -149,7 +170,7 @@ return {
 		"^[Hh]i$",
 		"^[Hh]ello$",
 		"^[Ss]pam$",
-		"^ZAC$",
+		"^[Ff]gp$",
 		"^[Bb]ot$",
 		"^[Uu]mbrella$",
 		"^[Bb]ye$",
