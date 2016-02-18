68acaae196e6f1ecf988d6d3b4ea73a84f8e25ac
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-02-18 18:25:52 +0330

    Update xy.lua

--- plugins/xy.lua
+++ plugins/xy.lua
@@ -2,11 +2,19 @@ do
 local function run(msg, matches)
 local x = 125775990
  if msg.text == "xy on" or msg.text == "Xy on" then
+ if is_sudo(msg) then
     xy = 1
-    return "on"
+    return "on✔"
+ else
+ return "این دستور فقط برای سودو است"
+ end
  elseif msg.test == "xy off" or msg.text == "Xy off" then
+ if is_sudo(msg) then
     xy = 0
-    return "off"
+    return "off❌"
+ else
+ return "این دستور فقط برای سودو است"
+ end
  end
     if msg.action.type == "chat_del_user" and msg.action.user.id == tonumber(x) then
        if tonumber(xy) == 1 then
