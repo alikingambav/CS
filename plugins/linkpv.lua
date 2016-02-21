5707d09f18ee793a2079393c008d6900f193e36a
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-02-21 09:04:49 +0330

    Create linkpv.lua

---
+++ plugins/linkpv.lua
@@ -0,0 +1,20 @@
+local function run(msg, matches)
+
+    if matches[1] == 'linkpv' or matches[1] == 'Linkpv'then
+      if not is_momod(msg) then
+        return "For moderators only!"
+      end
+      local group_link = data[tostring(msg.to.id)]['settings']['set_link']
+      if not group_link then 
+        return "Create a link first !"
+      end
+       savelog(msg.to.id, name_log.." ["..msg.from.id.."] requested group link ["..group_link.."]")
+     send_large_msg('user#id'..msg.from.id, "Group link:\n"..group_link)
+    end
+end
+return {
+  patterns = {
+  "^([Ll]inkpv)$",
+  },
+  run = run
+}
