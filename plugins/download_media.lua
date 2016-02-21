8311326adc3d94fb0a6b5e66a00c1308fc6e92f8
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-02-21 09:37:59 +0330

    Update download_media.lua

--- plugins/download_media.lua
+++ plugins/download_media.lua
@@ -1,59 +1,33 @@
-do
+local function callback(extra, success, result) -- Calback for load_photo in line 17
+  if success then
+    print('File downloaded to:', result)
+  else
+    print('Error downloading: '..extra)
+  end
+end
 
 local function run(msg, matches)
-  local receiver = get_receiver(msg)
-  local url = matches[1]
-  local ext = matches[2]
-
-  local file = download_to_file(url)
-  local cb_extra = {file_path=file}
-  
-  local mime_type = mimetype.get_content_type_no_sub(ext)
-
-  if ext == 'gif' then
-    print('send_file')
-    send_document(receiver, file, rmtmp_cb, cb_extra)
-
-  elseif mime_type == 'text' then
-    print('send_document')
-    send_document(receiver, file, rmtmp_cb, cb_extra)
-  
-  elseif mime_type == 'image' then
-    print('send_photo')
-    send_photo(receiver, file, rmtmp_cb, cb_extra)
-  
-  elseif mime_type == 'audio' then
-    print('send_audio')
-    send_audio(receiver, file, rmtmp_cb, cb_extra)
-
-  elseif mime_type == 'video' then
-    print('send_video')
-    send_video(receiver, file, rmtmp_cb, cb_extra)
-  
-  else
-    print('send_file')
-    send_file(receiver, file, rmtmp_cb, cb_extra)
+  if not is_momod(msg) then -- Will download images only from mods,owner and admins
+    return
   end
-  
+  if msg.media then
+    if msg.media.type == 'photo' then
+      load_photo(msg.id, callback, msg.id)
+    end
+  end
+end
+
+local function pre_process(msg)
+  if not msg.text and msg.media then
+    msg.text = '['..msg.media.type..']'
+  end
+  return msg
 end
 
 return {
-  description = "When user sends media URL (ends with gif, mp4, pdf, etc.) download and send it to origin.", 
-  usage = "When user sends media URL (ends with gif, mp4, pdf, etc.) download and send it to origin.",
+  run = run,
   patterns = {
-    "(https?://[%w-_%.%?%.:/%+=&]+%.(gif))$",
-    "(https?://[%w-_%.%?%.:/%+=&]+%.(mp4))$",
-    "(https?://[%w-_%.%?%.:/%+=&]+%.(pdf))$",
-    "(https?://[%w-_%.%?%.:/%+=&]+%.(ogg))$",
-    "(https?://[%w-_%.%?%.:/%+=&]+%.(zip))$",
-    "(https?://[%w-_%.%?%.:/%+=&]+%.(mp3))$",
-    "(https?://[%w-_%.%?%.:/%+=&]+%.(rar))$",
-    "(https?://[%w-_%.%?%.:/%+=&]+%.(wmv))$",
-    "(https?://[%w-_%.%?%.:/%+=&]+%.(doc))$",
-    "(https?://[%w-_%.%?%.:/%+=&]+%.(avi))$",
-    "(https?://[%w-_%.%?%.:/%+=&]+%.(webp))$"
-  }, 
-  run = run 
+    '%[(photo)%]'
+	},
+  pre_process = pre_process
 }
-
-end
