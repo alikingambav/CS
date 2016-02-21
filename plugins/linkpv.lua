21c8ecb5b12849536fe60e3a61511c40d8ca829f
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-02-21 10:00:06 +0330

    Update linkpv.lua

--- plugins/linkpv.lua
+++ plugins/linkpv.lua
@@ -1,6 +1,9 @@
 local function run(msg, matches)
-
+    
     if matches[1] == 'linkpv' or matches[1] == 'Linkpv'then
+      if not is_momod(msg) then
+        return "For moderators only!"
+       end
       local group_link = data[tostring(msg.to.id)]['settings']['set_link']
       local name_log = user_print_name(msg.from)
       if not group_link then 
