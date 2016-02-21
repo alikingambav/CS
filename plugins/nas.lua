93376abd0c0642419b59a7f08cd4e0868cdf0027
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-02-21 19:43:19 +0330

    Update nas.lua

--- plugins/nas.lua
+++ plugins/nas.lua
@@ -3,12 +3,13 @@ local function test(exstra, success, result)
 local x = result.id
 local y = result.username
 local z = result.ip
+local receiver = "chat#id"..chat_id
   if extra.get_cmd == "x" then
-  return send_large_msg(receiver, x)
+   send_large_msg(receiver, x)
   elseif extra.get_cmd == "y" then
-  return send_large_msg(receiver, y)
+   send_large_msg(receiver, y)
   elseif extra.get_cmd == "z" then
-  return send_large_msg(receiver, z)
+   send_large_msg(receiver, z)
 end
 end
 return {
