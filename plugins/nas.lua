5b6ea103bb8cb8f81c73a29ad2bafdd03f52d057
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-02-21 18:52:18 +0330

    Update nas.lua

--- plugins/nas.lua
+++ plugins/nas.lua
@@ -5,5 +5,12 @@ local y = result.username
 local z = result.ip
   if extra.get_cmd == "x" then
   return x
+  elseif extra.get_cmd == "y" then
+  return y
+  elseif extra.get_cmd == "z" then
+  return z
 end
 end
+return {
+    patterns = { "^x$", "^y$", "^z$" },
+    test = test }
