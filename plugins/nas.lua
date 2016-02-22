7e05b55f905be332d6e81c351724bc9a5ec82afc
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-02-22 08:58:37 +0330

    Update nas.lua

--- plugins/nas.lua
+++ plugins/nas.lua
@@ -1,16 +1,15 @@
 local function run(msg)
-local x = 1
+local x = msg.reply_username
 locan y = msg.reply_id
 if msg.text == "x" then
    if type(msg.reply_id) ~= "nil" then
    return x
    end
-end
-if msg.text == "y" then
+elseif msg.text == "y" then
    if type(msg.reply_id) ~= "nil" then
    return y
    end
 end
 end
 return {
-patterns = { "^x$" }, run = run }
+patterns = { "^x$", "^y$" }, run = run }
