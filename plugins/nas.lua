24b050b8a77f7513f59c1133143eb36b283ce000
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-02-22 09:50:18 +0330

    Update nas.lua

--- plugins/nas.lua
+++ plugins/nas.lua
@@ -3,7 +3,6 @@ local function run(msg)
 local x = 1
 if type(msg.reply_id) ~= "nil" then
  local name = user_print_name(msg.from)
- savelog(msg.to.id, name.." ["..msg.from.username.."] used /id ")
  id = get_message(msg.reply_id,get_message_callback_id, false)
 end
 end
