41fb206d5fc72a118255d659745f21a7a6fde691
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-02-22 09:25:18 +0330

    Update nas.lua

--- plugins/nas.lua
+++ plugins/nas.lua
@@ -1,5 +1,14 @@
+local function test(extra, success, result)
+local un = result.username
+local id = result.id
+local receiver = "chat#id"..chat_id
+if get_cmd = "y" then
+return send_large_msg(receiver, y)
+end
+end
+
 local function run(msg)
-local x = msg.reply_ip
+local x = 1
 local y = msg.reply_id
 if msg.text == "x" then
    if type(msg.reply_id) ~= "nil" then
