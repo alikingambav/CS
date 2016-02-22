1fbcde8144f3cf131f1e7c492d36638a405f0d26
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-02-22 09:54:24 +0330

    Update nas.lua

--- plugins/nas.lua
+++ plugins/nas.lua
@@ -2,7 +2,7 @@
 local function run(msg)
 local x = 1
 if type(msg.reply_id) ~= "nil" then
-get_message(msg.reply_id,get_message_callback_username, false)
+get_message(msg.reply_id,get_message_callback_ip, false)
 end
 end
 return {
