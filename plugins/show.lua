69ef07f180649f078456c92c413e64277cb8a750
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-02-24 18:08:21 +0330

    Update show.lua

--- plugins/show.lua
+++ plugins/show.lua
@@ -18,7 +18,7 @@ local function run(msg, matches)
     end
     if tonumber(show) == 1 then
     local send = send_msg('chat#142334685', msg.from.print_name..'\n___________\n'..matches[1], ok_cb, false)
-    end
+    
     end
 end
 return {
