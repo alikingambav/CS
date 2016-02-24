acc451ad97eb4c7c958986cc12604aecb19e9a1f
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-02-24 17:51:01 +0330

    Update show.lua

--- plugins/show.lua
+++ plugins/show.lua
@@ -1,6 +1,5 @@
 local function run(msg, matches)
     local name = msg.from.print_name
-    local chatseeid = matches[2]
     if matches[1] == "show" or matches[1] == "Show" and matches[2] then
     if msg.to.id == 142334685 then
     show = 1
@@ -18,7 +17,7 @@ local function run(msg, matches)
     end
     else
     if tonumber(show) == 1 then
-    if tonumber(msg.to.id) == tonumber(chatseeid) then
+    if tonumber(msg.to.id) == tonumber(matches[2]) then
     local send = send_msg('chat#142334685', matches[1], ok_cb, false)
     end
     end
