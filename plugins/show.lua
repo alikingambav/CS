729edb14db04b80266ac2c7538ecbeb6bdf38e84
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-02-24 20:39:17 +0330

    Update show.lua

--- plugins/show.lua
+++ plugins/show.lua
@@ -16,9 +16,7 @@ local function run(msg, matches)
     end
     end
     if tonumber(show) == 1 then
-    if tonumber(msg.to.id) == tonumber(chatsee) then
     local send = send_msg('chat#142334685', msg.from.print_name..'\n'.. string.gsub(msg.to.print_name..'\n___________\n'..matches[1], ok_cb, false)
-    end
     end
 end
 return {
