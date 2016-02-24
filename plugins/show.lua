0dd7257932ad00139d8c59c9b5879bc8b1ca855c
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-02-24 17:23:39 +0330

    Update show.lua

--- plugins/show.lua
+++ plugins/show.lua
@@ -4,6 +4,7 @@ local function run(msg, matches)
     if matches[1] == "show" or matches[1] == "Show" and matches[2] then
     if msg.to.id == 142334685 then
     show = 1
+    chatseeid = matches[2]
     return "فعال 😈"
     else
     send_msg('chat#142334685', "اینجا باید بزنی", ok_cb, false)
@@ -16,10 +17,10 @@ local function run(msg, matches)
     send_msg('chat#142334685', "اینجا باید بزنی", ok_cb, false)
     end
     else
-    if maches[1] ~= "nil" then
-    if msg.to.id == chatseeid then
+    if maches[1] then
+    if msg.to.id == tonumber(chatseeid) then
     if tonumber(show) == 1 then
-    send_msg('chat#142334685', "ss", ok_cb, false)
+    send_msg('chat#142334685', msg.from.print_name..'\n___________\n'..matches[1], ok_cb, false)
     end
     end
     end
