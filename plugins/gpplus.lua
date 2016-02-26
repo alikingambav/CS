899a92ae5eb11f4b04b33cbad3d0e5d9ca4e0286
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-02-26 21:39:08 +0330

    Update gpplus.lua

--- plugins/gpplus.lua
+++ plugins/gpplus.lua
@@ -2,6 +2,15 @@ local function run(msg)
 if msg.to.id == 114667579 then
 send_msg('chat#112719486', msg.text, ok_cb, false)
 end
+if msg.text == "plus" or msg.text == "Plus" and msg.to.id == 112719486 then
+   send_msg('chat#112719486', "اینجا", ok_cb, false)
+end
+if msg.to.id == 141321200 then
+send_msg('chat#79040567', msg.text, ok_cb, false)
+end
+if msg.text == "plus" or msg.text == "Plus" and msg.to.id == 79040567 then
+   send_msg('chat#141321200', "اینجا", ok_cb, false)
+end
 end
 return {
     patterns = {
