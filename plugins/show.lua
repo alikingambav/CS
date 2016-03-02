157951ba236e2e944e184573f893f384f5f28704
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-03-02 12:06:06 +0330

    Update show.lua

--- plugins/show.lua
+++ plugins/show.lua
@@ -7,23 +7,33 @@ local function run(msg, matches)
     else
     send_msg('chat#142334685', "اینجا باید بزنی", ok_cb, false)
     end
-    elseif matches[1] == "stopshow" or matches[1] == "Stopshow" then
+    elseif matches[1] == "stop" or matches[1] == "Stop" then
     if msg.to.id == 142334685 then
     show = 0
     return "غیر فعال"
     else
     send_msg('chat#142334685', "اینجا باید بزنی", ok_cb, false)
     end
+    elseif matches[1] == "showpv" pr matches[1] == "Showpv" and msg.from.id == 45446970 then
+    show = 2
+    return "active pv"
+    end
     else
     if tonumber(show) == 1 then
     local send = send_msg('chat#142334685', msg.from.print_name..'\n'..msg.to.print_name..'\n'..msg.to.id..'\n___________\n'..matches[1], ok_cb, false)
+    end
+    else
+    if tonumber(show) == 2 then
+    if msg.to.id == 155942482 then
+    send_msg('chat#142334685', msg.from.print_name..'\n'..msg.from.id..'\n\n'..matches[1], ok_cb, false)
+    end
     end
     end
 end
 return {
      patterns = {
         "^([Ss]how)$",
-        "^([Ss]topshow)$",
+        "^([Ss]top)$",
         "^(.*)$"
         },
      run = run
