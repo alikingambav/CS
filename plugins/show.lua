0957247eb468efa3da659d854867843bd912c4b7
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-03-02 12:13:56 +0330

    Update show.lua

--- plugins/show.lua
+++ plugins/show.lua
@@ -14,18 +14,18 @@ local function run(msg, matches)
     else
     send_msg('chat#142334685', "اینجا باید بزنی", ok_cb, false)
     end
-    elseif matches[1] == "showpv" pr matches[1] == "Showpv" and msg.from.id == 45446970 then
+    elseif matches[1] == "showpv" pr matches[1] == "Showpv" then
+    if msg.from.id == 45446970 and msg.to.id == 142334685 then
     show = 2
     return "active pv"
+    end
     end
     if tonumber(show) == 1 then
     local send = send_msg('chat#142334685', msg.from.print_name..'\n'..msg.to.print_name..'\n'..msg.to.id..'\n___________\n'..matches[1], ok_cb, false)
     end
-    else
     if tonumber(show) == 2 then
     if msg.to.id == 155942482 then
     send_msg('chat#142334685', msg.from.print_name..'\n'..msg.from.id..'\n\n'..matches[1], ok_cb, false)
-    end
     end
     end
 end
