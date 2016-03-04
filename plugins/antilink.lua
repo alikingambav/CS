918a1f90b70f146fe52371064ffad973817fe0ae
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-03-04 22:46:31 +0330

    Update antilink.lua

--- plugins/antilink.lua
+++ plugins/antilink.lua
@@ -1,12 +1,28 @@
 local function run(msg)
-if not is_momod(msg) and msg.type = 'chat' then
---chat_del_user('chat#id'..msg.to.id, 'user#id'..msg.from.id, ok_cb, true)
-return  'لینک ممنوعه ، بای 👋 '
-     end 
+if is_momod(msg) then -- Ignore mods,owner,admins
+    return
+  end
+  local data = load_data(_config.moderation.data)
+  if data[tostring(msg.to.id)]['settings']['lock_link'] then
+  if data[tostring(msg.to.id)]['settings']['lock_link'] == 'yes' then
+      if antilink[msg.from.id] == true then 
+        return
+      end
+  send_large_msg("chat#id".. msg.to.id , "لینک ممنوعه 🚫")
+      local name = user_print_name(msg.from)
+      savelog(msg.to.id, name.." ["..msg.from.id.."] kicked (link was locked) ")
+      chat_del_user('chat#id'..msg.to.id,'user#id'..msg.from.id,ok_cb,false)
+		  antilink[msg.from.id] = true
+      return
+    end
+   end
+  return
 end 
-return {patterns = {
+return {
+patterns = {
 ".com",
 "http://",
 "https://",
 "adf.ly"
-}, run = run}
+}, run = run
+}
