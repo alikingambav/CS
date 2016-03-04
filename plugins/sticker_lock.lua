666370e658c61c1e80e57d4cc3be121d264e5bb9
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-03-04 21:52:14 +0330

    Update sticker_lock.lua

--- plugins/sticker_lock.lua
+++ plugins/sticker_lock.lua
@@ -4,26 +4,24 @@ local function run(msg, matches)
 if not is_momod(msg) or not is_sudo(msg) or not is_admin(msg) then
 	return "Mods Only!"
 	else
-    if matches[1] == 'sticker' or matches[1] == 'Sticker' then
-      if matches[2] == 'warn' then
+    if matches[1] == 'Warn' or matches[1] == 'warn' then
+      if matches[2] == 'sticker' then
         if welcome_stat ~= 'warn' then
           data[tostring(msg.to.id)]['settings']['sticker'] = 'warn'
           save_data(_config.moderation.data, data)
         end
         return '[Alredy Enabled]\nSticker Sender will be warned first, then kicked for second Sticker.'
-      end
-      if matches[2] == 'kick' then
-        if welcome_stat ~= 'kick' then
-          data[tostring(msg.to.id)]['settings']['sticker'] = 'kick'
+      elseif matches[1] == 'lock' or matches[1] == 'Lock' then
+        if welcome_stat ~= 'yes' then
+          data[tostring(msg.to.id)]['settings']['sticker'] = 'yes'
           save_data(_config.moderation.data, data)
         end
         return '[Already Enabled]Sticker Sender will be kicked!'
-      end
-      if matches[2] == 'ok' then
-        if welcome_stat == 'ok' then
+      elseif matches[1] == 'Unlock' or matches[1] == 'unlock' then
+        if welcome_stat == 'no' then
           return '[Already Disabled]Nothing Will Happend If Sticker Sent!'
         else
-          data[tostring(msg.to.id)]['settings']['sticker'] = 'ok'
+          data[tostring(msg.to.id)]['settings']['sticker'] = 'no'
           save_data(_config.moderation.data, data)
           return 'Nothing Will Happend If Sticker Sent! '
         end
@@ -35,7 +33,7 @@ end
 
 return {
      patterns = {
-         "^([Ss]ticker) (.*)$"
+         "^([Ll]ock) (.*)$"
      },
 run = run,
 pre_process = pre_process
