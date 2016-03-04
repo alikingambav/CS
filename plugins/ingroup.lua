ddc3dd9b1819b20174c0ddf0c4ddb43dbf2cf1fd
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-03-04 20:09:12 +0330

    Update ingroup.lua

--- plugins/ingroup.lua
+++ plugins/ingroup.lua
@@ -181,32 +181,6 @@ local function check_member_modrem(cb_extra, success, result)
   end
 end
 --End Check Member
-local function show_group_settingsmod(msg, data, target)
- 	if not is_momod(msg) then
-    	return "For moderators only!"
-  	end
-  	local data = load_data(_config.moderation.data)
-    if data[tostring(msg.to.id)] then
-     	if data[tostring(msg.to.id)]['settings']['flood_msg_max'] then
-        	NUM_MSG_MAX = tonumber(data[tostring(msg.to.id)]['settings']['flood_msg_max'])
-        	print('custom'..NUM_MSG_MAX)
-      	else 
-        	NUM_MSG_MAX = 5
-      	end
-    end
-    local bots_protection = "Yes"
-    if data[tostring(msg.to.id)]['settings']['lock_bots'] then
-    	bots_protection = data[tostring(msg.to.id)]['settings']['lock_bots']
-   	end
-    local leave_ban = "no"
-    if data[tostring(msg.to.id)]['settings']['leave_ban'] then
-    	leave_ban = data[tostring(msg.to.id)]['settings']['leave_ban']
-   	end
-  local settings = data[tostring(target)]['settings']
-  local text = "Group settings:\nLock group name : "..settings.lock_name.."\nLock group photo : "..settings.lock_photo.."\nLock group member : "..settings.lock_member.."\nLock group leave : "..leave_ban.."\nflood sensitivity : "..NUM_MSG_MAX.."\nBot protection : "..bots_protection--"\nPublic: "..public
-  return text
-end
-
 local function set_descriptionmod(msg, data, target, about)
   if not is_momod(msg) then
     return "For moderators only!"
@@ -1034,11 +1008,7 @@ local function run(msg, matches)
        return unlock_group_leave(msg, data, target)
      end
    end
-    if matches[1] == 'settings' or matches[1] == 'Settings' then
-      local target = msg.to.id
-      savelog(msg.to.id, name_log.." ["..msg.from.id.."] requested group settings ")
-      return show_group_settingsmod(msg, data, target)
-    end	
+    
 
   --[[if matches[1] == 'public' or matches[1] == 'Public' then
     local target = msg.to.id
@@ -1250,7 +1220,6 @@ return {
   "^([Ss]etgpowner) (%d+) (%d+)$",-- (group id) (owner id)
   "^([Uu]nlock) (.*)$",
   "^([Ss]etflood) (%d+)$",
-  "^([Ss]ettings)$",
 -- "^([Pp]ublic) (.*)$",
   "^([Mm]odlist)$",
   "^([Nn]ewlink)$",
