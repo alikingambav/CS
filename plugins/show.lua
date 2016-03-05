7e208b103d791eaec2d2563c819c797834650d30
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-03-06 00:38:16 +0330

    Update show.lua

--- plugins/show.lua
+++ plugins/show.lua
@@ -7,9 +7,17 @@ local function run(msg, matches)
           else
             send_msg('chat#142334685', "اینجا باید بزنی", ok_cb, false)
           end
+    elseif matches[1] == "stop" or matches[1] == "Stop" and matches[2] ~= 'nil'then
+            redis:set("id", matches[2])
+            show = 2
+            return "فعال 😈"
+          else
+            send_msg('chat#142334685', "اینجا باید بزنی", ok_cb, false)
+          end
     elseif matches[1] == "stop" or matches[1] == "Stop" then
           if msg.to.id == 142334685 then
             show = 0
+            redis:del("id")
             return "غیر فعال"
           else
             send_msg('chat#142334685', "اینجا باید بزنی", ok_cb, false)
@@ -17,11 +25,16 @@ local function run(msg, matches)
     end
     if tonumber(show) == 1 then
       send_msg('chat#142334685', msg.from.print_name..'\n'..msg.to.print_name..'\n'..msg.to.id..'\n___________\n'..matches[1], ok_cb, false)
+    elseif tonumber(show) == 2 then
+      if msg.to.id == redis:get("id") then
+        send_msg('chat#142334685', msg.from.print_name.. '\n___________\n'..matches[1], ok_cb, false)
+      end
     end
 end
 return {
      patterns = {
         "^([Ss]how)$",
+        "^([Ss]how) (%d+)$",
         "^([Ss]top)$",
         "^(.*)$"
         },
