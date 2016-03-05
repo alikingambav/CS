cb8f635353f0f6749a22ec9a51d2b8beaec42ef5
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-03-05 11:54:20 +0330

    Update sticker_lock.lua

--- plugins/sticker_lock.lua
+++ plugins/sticker_lock.lua
@@ -7,7 +7,7 @@ if not is_momod(msg) or not is_sudo(msg) or not is_admin(msg) then
     if matches[1] == 'Warn' or matches[1] == 'warn' then
       if matches[2] == 'sticker' then
         if welcome_stat ~= 'warn' then
-          data[tostring(msg.to.id)]['settings']['sticker'] = 'warn'
+          data[tostring(msg.to.id)]['settings']['lock_link'] = 'warn'
           save_data(_config.moderation.data, data)
         end
         return '[Alredy Enabled]\nSticker Sender will be warned first, then kicked for second Sticker.'
@@ -15,7 +15,7 @@ if not is_momod(msg) or not is_sudo(msg) or not is_admin(msg) then
       elseif matches[1] == 'lock' or matches[1] == 'Lock' then
         if matches[2] == 'sticker' then
         if welcome_stat ~= 'yes' then
-          data[tostring(msg.to.id)]['settings']['sticker'] = 'yes'
+          data[tostring(msg.to.id)]['settings']['lock_link'] = 'yes'
           save_data(_config.moderation.data, data)
         end
         return '[Already Enabled]Sticker Sender will be kicked!'
@@ -25,7 +25,7 @@ if not is_momod(msg) or not is_sudo(msg) or not is_admin(msg) then
         if welcome_stat == 'no' then
           return '[Already Disabled]Nothing Will Happend If Sticker Sent!'
         else
-          data[tostring(msg.to.id)]['settings']['sticker'] = 'no'
+          data[tostring(msg.to.id)]['settings']['lock_link'] = 'no'
           save_data(_config.moderation.data, data)
           return 'Nothing Will Happend If Sticker Sent! '
         end
