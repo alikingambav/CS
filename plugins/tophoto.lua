1fab156a3da495d579b4508ff330214261219669
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-03-06 00:18:59 +0330

    Update tophoto.lua

--- plugins/tophoto.lua
+++ plugins/tophoto.lua
@@ -6,22 +6,25 @@ local function tosticker(msg, success, result)
     os.rename(result, file)
     print('File moved to:', file)
     send_document(get_receiver(msg), file, ok_cb, false)
-    redis:del("sticker:photo")
+    data[tostring(msg.to.id)]['settings']['stf'] = 'off'
+    save_data(_config.moderation.data, data)
   else
     print('Error downloading: '..msg.id)
     send_large_msg(receiver, 'Failed, please try again!', ok_cb, false)
   end
 end
 local function run(msg,matches)
+local stf = data[tostring(msg.to.id)]['settings']['stf']
     local receiver = get_receiver(msg)
     local group = msg.to.id
-      	if msg.from.type == 'document' and is_momod(msg) and redis:get("sticker:photo") then
-      		if redis:get("sticker:photo") == 'waiting' then
-        		load_document(msg.id, tosticker, msg)
-      		end
+    if msg.media then
+      	if msg.media.type == 'document' and is_momod(msg) and stf == 'waiting' then
+              load_document(msg.id, tosticker, msg)
       	end
+    end
     if matches[1] == "tophoto" and is_momod(msg) then
-    	redis:set("sticker:photo", "waiting")
+    data[tostring(msg.to.id)]['settings']['stf'] = 'waiting'
+    save_data(_config.moderation.data, data)
     	return 'Please send your sticker now'
     end
 end
