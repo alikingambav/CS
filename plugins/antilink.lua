4c5f25f2d99e07b7ca6d207e780b2f9cfd976335
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-03-05 10:56:16 +0330

    Update antilink.lua

--- plugins/antilink.lua
+++ plugins/antilink.lua
@@ -1,65 +1,26 @@
-local function lock_group_link(msg, data, target)
-if not is_momod(msg) then
-    return "For moderators only!"
-  end
-  local group_link_lock = data[tostring(target)]['settings']['lock_link']
-  if group_arabic_lock == 'yes' then
-    return 'Link is already locked'
-  else
-    data[tostring(target)]['settings']['lock_link'] = 'yes'
-    save_data(_config.moderation.data, data)
-    return 'link has been locked'
-  end
-end
-local function unlock_group_link(msg, data, target)
-if not is_momod(msg) then
-    return "For moderators only!"
-  end
-  local group_link_lock = data[tostring(target)]['settings']['lock_link']
-  if group_arabic_lock == 'no' then
-    return 'Link is already unlocked'
-  else
-    data[tostring(target)]['settings']['lock_link'] = 'no'
-    save_data(_config.moderation.data, data)
-    return 'link has been unlocked'
-  end
-end
 local function run(msg, matches)
-local data = load_data(_config.moderation.data)
-local name_log = user_print_name(msg.from)
-if matches[1] == 'Lock'or matches [1] == 'Lock'then
-if matches[2] == 'link'then
-savelog(msg.to.id, name_log.." ["..msg.from.id.."] locked link ")
-        return lock_group_link(msg, data, target)
-end
-elseif matches[1] == 'Unlock'or matches [1] == 'unlock'then
-if matches[2] == 'link'then
-savelog(msg.to.id, name_log.." ["..msg.from.id.."] locked link ")
-        return unlock_group_link(msg, data, target)
-end
-end
--------
-if is_momod(msg) then -- Ignore mods,owner,admins
-    return
-  end
-  if data[tostring(msg.to.id)]['settings']['lock_link'] then
-    if data[tostring(msg.to.id)]['settings']['lock_link'] == 'yes' then
-      if antilink[msg.from.id] == true then 
-        return
-      end
-      send_large_msg("chat#id".. msg.to.id , "link is not allowed here")
-      local name = user_print_name(msg.from)
-      savelog(msg.to.id, name.." ["..msg.from.id.."] kicked (link was locked) ")
-      chat_del_user('chat#id'..msg.to.id,'user#id'..msg.from.id,ok_cb,false)
-		  antilink[msg.from.id] = true
-      return
-    end
-  end
--------
-  return
-end
-local function cron()
-  antiarabic = {} -- Clear antiarabic table 
+
+
+if not is_momod(msg) or not is_sudo(msg) or not is_admin(msg) then
+	return "Mods Only!"
+	else
+    if matches[1] == 'Lock' or matches[1] == 'lock' then
+      if matches[2] == 'link' then
+        if welcome_stat ~= 'yes' then
+          data[tostring(msg.to.id)]['settings']['lock_link'] = 'yes'
+          save_data(_config.moderation.data, data)
+        end
+        return 'link locked'
+        end
+      elseif matches[1] == 'Unlock' or matches[1] == 'unlock' then
+        if matches[2] == 'link' then
+        if welcome_stat ~= 'no' then
+          data[tostring(msg.to.id)]['settings']['lock_link'] = 'no'
+          save_data(_config.moderation.data, data)
+        end
+        return 'link unlocked'
+        end
+     end
 end
 return {
 patterns = {
