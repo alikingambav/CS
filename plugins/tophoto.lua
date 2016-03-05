c86d59a25ce618632c31d2a6e557f25556b667de
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-03-06 00:24:46 +0330

    Update tophoto.lua

--- plugins/tophoto.lua
+++ plugins/tophoto.lua
@@ -5,32 +5,31 @@ local function tosticker(msg, success, result)
     print('File downloaded to:', result)
     os.rename(result, file)
     print('File moved to:', file)
-    send_document(get_receiver(msg), file, ok_cb, false)
-    data[tostring(msg.to.id)]['settings']['stf'] = 'off'
-    save_data(_config.moderation.data, data)
+    send_photo(get_receiver(msg), file, ok_cb, false)
+    redis:del("sticker:photo")
   else
     print('Error downloading: '..msg.id)
     send_large_msg(receiver, 'Failed, please try again!', ok_cb, false)
   end
 end
 local function run(msg,matches)
-local stf = data[tostring(msg.to.id)]['settings']['stf']
     local receiver = get_receiver(msg)
     local group = msg.to.id
     if msg.media then
-      	if msg.media.type == 'document' and is_momod(msg) and stf == 'waiting' then
-              load_document(msg.id, tosticker, msg)
+      	if msg.media.type == 'document' and is_momod(msg) and redis:get("sticker:photo") then
+      		if redis:get("sticker:photo") == 'waiting' then
+        		load_document(msg.id, tosticker, msg)
+      		end
       	end
     end
     if matches[1] == "tophoto" and is_momod(msg) then
-    data[tostring(msg.to.id)]['settings']['stf'] = 'waiting'
-    save_data(_config.moderation.data, data)
+    	redis:set("sticker:photo", "waiting")
     	return 'Please send your sticker now'
     end
 end
 return {
   patterns = {
-	"^([Tt]ophoto)$",
+	"^(tophoto)$",
 	"%[(document)%]",
   },
   run = run,
