24d16d3b9307821f30ef2a823ad16ba2fa22fd34
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-03-05 12:00:01 +0330

    Update antilink.lua

--- plugins/antilink.lua
+++ plugins/antilink.lua
@@ -1,26 +1,37 @@
 local function run(msg, matches)
-if not is_momod(msg) then
+
+
+if not is_momod(msg) or not is_sudo(msg) or not is_admin(msg) then
 	return "Mods Only!"
 	else
-    if matches[1] == 'Lock' or matches[1] == 'lock' then
-      if matches[2] == 'link' then
+        if matches[1] == 'lock' or matches[1] == 'Lock' then
+        if matches[2] == 'link' then
+        if welcome_stat ~= 'yes' then
           data[tostring(msg.to.id)]['settings']['lock_link'] = 'yes'
           save_data(_config.moderation.data, data)
+        end
         return 'link locked'
         end
       elseif matches[1] == 'Unlock' or matches[1] == 'unlock' then
-        if matches[2] == 'link' then
+           if matches[2] == 'link' then
+        if welcome_stat == 'no' then
+          return 'link unlocked'
+        else
           data[tostring(msg.to.id)]['settings']['lock_link'] = 'no'
           save_data(_config.moderation.data, data)
-        return 'link unlocked'
+          return 'Nothing Will Happend If Sticker Sent! '
         end
-     end
+      end
+    end
 end
+end
+
+
 return {
-patterns = {
-"^([Ll]ock) (link)$",
-"^([Uu]nlock) (link)$"
-},
- run = run,
- pre_process = pre_process
+     patterns = {
+         "^([Ll]ock) (link)$",
+         "^([Uu]nlock) (link)$"
+     },
+run = run,
+pre_process = pre_process
 }
