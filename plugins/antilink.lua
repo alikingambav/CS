dd61af27fe38bf0487c5344f4cb2deb3767ad08e
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-03-05 11:42:46 +0330

    Update antilink.lua

--- plugins/antilink.lua
+++ plugins/antilink.lua
@@ -1,12 +1,12 @@
 local function run(msg, matches)
 
-
-if not is_momod(msg) or not is_sudo(msg) or not is_admin(msg) then
+local locklink = data[tostring(msg.to.id)]['settings']['lock_link']
+if not is_momod(msg) then
 	return "Mods Only!"
 	else
     if matches[1] == 'Lock' or matches[1] == 'lock' then
       if matches[2] == 'link' then
-        if welcome_stat ~= 'yes' then
+        if locklink ~= 'yes' then
           data[tostring(msg.to.id)]['settings']['lock_link'] = 'yes'
           save_data(_config.moderation.data, data)
         end
@@ -14,7 +14,7 @@ if not is_momod(msg) or not is_sudo(msg) or not is_admin(msg) then
         end
       elseif matches[1] == 'Unlock' or matches[1] == 'unlock' then
         if matches[2] == 'link' then
-        if welcome_stat ~= 'no' then
+        if locklink ~= 'no' then
           data[tostring(msg.to.id)]['settings']['lock_link'] = 'no'
           save_data(_config.moderation.data, data)
         end
