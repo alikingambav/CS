79e449156056bc11efbf302742789eb4b04578a8
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-03-06 10:52:27 +0330

    Update show.lua

--- plugins/show.lua
+++ plugins/show.lua
@@ -1,5 +1,5 @@
 local function run(msg, matches)
-local botid = "155942482"
+local botid = 155942482
     if matches[1] == "show" or matches[1] == "Show" and not matches[2] then
             if msg.to.id == 142334685 then
               show = 1
