8734a58ee1ac46ab3f3efeedd7fcf5ef8646d6d3
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-03-06 09:38:33 +0330

    Update show.lua

--- plugins/show.lua
+++ plugins/show.lua
@@ -21,7 +21,7 @@ local function run(msg, matches)
             else
               send_msg('chat#142334685', "اینجا باید بزنی", ok_cb, false)
             end
-    elseif matches[1] == "showpv" or matches[1] == "Showpv" and matches[2] ~= 'nil' then
+    elseif matches[1] == "showpv" or matches[1] == "Showpv" and matches[2] then
           if msg.to.id == 142334685 then
               redis:set("id", matches[2])
               show = 4
@@ -63,11 +63,11 @@ local function run(msg, matches)
         send_msg('chat#142334685', msg.from.print_name.. '\n___________\n'..matches[1], ok_cb, false)
       end
     elseif tonumber(show) == 3 then
-      if msg.to.type ~= 'chat'then
+      if msg.to.type ~= 'chat' then
         send_msg('chat#142334685', msg.from.print_name..'\n'..msg.to.id'\n___________\n'..matches[1], ok_cb, false)
       end
     elseif tonumber(show) == 4 then
-      if msg.to.type ~= 'chat'then
+      if msg.to.type ~= 'chat' then
         if msg.from.id == tonumber(redis:get("id")) then
           send_msg('chat#142334685', matches[1], ok_cb, false)
         end
