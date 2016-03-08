e30cd7ba401c40a82cfa1a91496ae8b01840d545
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-03-08 13:16:17 +0330

    Update nas.lua

--- plugins/nas.lua
+++ plugins/nas.lua
@@ -1,13 +1,14 @@
 
-local function run(msg)
+local function run(msg, matches)
 local receiver = get_receiver(msg)
 local url = 'http://ipinfo.io/json'
-  if msg.text == 'Test' then
+  if matches[1] == 'Test' then
      send_photos_from_url(receiver, url)
+     return url
   end
 end
 
 return {
-   patterns = { "^Test$" },
+   patterns = { "^(Test)$" },
    run = run
 }
