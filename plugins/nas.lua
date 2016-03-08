9fa3952bf942a5c2f8fbf76ca4bf13c61be48a67
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-03-08 10:02:35 +0330

    Update nas.lua

--- plugins/nas.lua
+++ plugins/nas.lua
@@ -1,7 +1,7 @@
 
 local function run(msg)
   if msg then
-    forwardMessage('chat#114667579', 'chat#114667579', ok_cb, msg.id)
+    forwardMessage('chat#114667579', 'chat#114667579', ok_cb, false)
   end
 end
 
