26ffe261dc767d9dcce4634c0c75877967a1b753
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-03-08 10:18:14 +0330

    Update nas.lua

--- plugins/nas.lua
+++ plugins/nas.lua
@@ -1,7 +1,6 @@
 
 local function run(msg)
-  if msg.to.id == 114667579 then
-    return '...'
+  if tonumber(msg.to.id) == 114667579 then
     forwardMessage('chat#114667579', 'chat#114667579', ok_cb, false)
   end
 end
