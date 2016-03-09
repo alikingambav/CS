ddc9ab9c0477063f7b60c4c6aa88db22cb58a0d8
fiallo1313veeee <aliarjmandi339@yahoo.com>
2016-03-09 14:19:29 +0330

    Update antilink.lua

--- plugins/antilink.lua
+++ plugins/antilink.lua
@@ -1,4 +1,13 @@
+local function kick_user(user_id, chat_id)
+  local chat = 'chat#id'..chat_id
+  local user = 'user#id'..user_id
+  chat_del_user(chat, user, function (data, success, result)
+  end, {chat=chat, user=user})
+end
+
 local function run(msg, matches)
+local user = msg.from.id
+ local chat = msg.to.id
 local locklink = data[tostring(msg.to.id)]['settings']['lock_link']
 if not is_momod(msg) or not is_sudo(msg) or not is_admin(msg) then
 	return "Mods Only!"
@@ -20,13 +29,36 @@ if not is_momod(msg) or not is_sudo(msg) or not is_admin(msg) then
          return 'link unlocked'
         end
       end
+      if locklink == 'yes' then
+       if msg.to.type ~= 'chat' then
+          return "Not a chat group!"
+          elseif user == tostring(our_id) then
+         --[[ A robot must protect its own existence as long as such protection does
+         not conflict with the First or Second Laws. ]]--
+         return ""
+         elseif not is_momod(msg) and not is_sudo(msg) then
+         kick_user(user, chat)
+         return "لینک ممنوعه 🚫"
+       end
+      end
+end
 end
 end
 
 return {
      patterns = {
          "^([Ll]ock) (link)$",
-         "^([Uu]nlock) (link)$"
+         "^([Uu]nlock) (link)$",
+         "[Hh][Tt][Tt][Pp][Ss]://[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]/",
+"[Hh][Tt][Tt][Pp][Ss]://[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]",
+"[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]/",
+"[Tt][Ee][Ll][Ee][Gg][Rr][Aa][Mm].[Mm][Ee]/[Jj][Oo][Ii][Nn][Cc][Hh][Aa][Tt]/",
+"[Hh][Tt][Tt][Pp]://",
+"[Ww][Ww][Ww]",
+".[Ii][Rr]",
+".[Cc][Oo][Mm]",
+"[Hh][Tt][Tt][Pp]://",
+"[Hh][Tt][Tt][Pp][Ss]://"
      },
 run = run,
 pre_process = pre_process
